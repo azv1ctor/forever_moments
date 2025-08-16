b16d9ee939a0877877852984d361201c6af28ba4
João Alves <joaovictoralvesazevedo@gmail.com>
2025-08-16 19:38:08 +0000

    segue dando erro ao tentar subir a foto para o feed, veio: 1/1
    Next.js

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,18 @@
 // /src/lib/firebase-admin.ts
 import admin from 'firebase-admin';
+import { config } from 'dotenv';
+
+// Carrega as variáveis de ambiente do arquivo .env
+config();
 
 // Garante que o SDK do Firebase Admin não seja inicializado múltiplas vezes.
 if (!admin.apps.length) {
   try {
+    // Verifica se as variáveis de ambiente necessárias estão presentes
+    if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+      throw new Error('As credenciais do Firebase Admin não estão definidas nas variáveis de ambiente.');
+    }
+    
     const serviceAccount: admin.ServiceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
